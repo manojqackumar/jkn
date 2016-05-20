b7a8e7cf70eadf1fb059f65f05d699afa4b815e8
manojqackumar <manoj.kumar.mohapatra@gmail.com>
2016-05-20 06:04:50 -0400

    Signed-off-by: manojqackumar <manoj.kumar.mohapatra@gmail.com>

--- src/test/java/testNG/TestngIntro1.java
+++ src/test/java/testNG/TestngIntro1.java
@@ -1,6 +1,8 @@
 package testNG;
 
 import org.testng.annotations.AfterMethod;
+import org.uncommons.reportng.HTMLReporter;
+import org.uncommons.reportng.JUnitXMLReporter;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.apache.log4j.Logger;
