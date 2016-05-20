65f115203c22dfc8a4d9ca0ca7f3578e2ae1a444
manojqackumar <manoj.kumar.mohapatra@gmail.com>
2016-05-20 06:43:26 -0400

    Signed-off-by: manojqackumar <manoj.kumar.mohapatra@gmail.com>

--- src/test/java/testNG/TestngIntro1.java
+++ src/test/java/testNG/TestngIntro1.java
@@ -5,25 +5,29 @@ import org.uncommons.reportng.HTMLReporter;
 import org.uncommons.reportng.JUnitXMLReporter;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 public class TestngIntro1 {
-	static Logger log= Logger.getLogger(TestngIntro1.class);
-	@BeforeMethod
+		static Logger log= Logger.getLogger(TestngIntro1.class);
+		@BeforeMethod
 	public void login()
 	{
+		BasicConfigurator.configure();
 		log.info("Executing the Test case Login");
 		System.out.println("Login to my System");
 	}
 	@Test
 	public void sendEmail()
 	{
+		BasicConfigurator.configure();
 		log.info("Executing the Test case sendEmail");
 		System.out.println("Send email");
 	}
 	@AfterMethod
 	public void logout()
 	{
+		BasicConfigurator.configure();
 		log.info("Executing the Test case Logout");
 		System.out.println("Logout from my system");
 	}
